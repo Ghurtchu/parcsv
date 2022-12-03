dd7b22906c38acf2e564c60d231718d5634a8fb4
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-12-03 22:55:07 +0400

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+The library is under active development
+
+Print result:
+
+
+![My Image](print_result.png)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,10 +5,10 @@ import csv.CSV
 object Main extends scala.App {
 
   val data = CSV("data/programming_languages.csv")
+
+  // Let's begin with simple task: display beautifully formatted CSV
   data match {
-    case Right(csv) => {
-      println(csv)
-    }
+    case Right(csv) => println(csv)
     case _ =>
   }
 
