188db75de0df4cf502f8ec02575095cc6a47235f
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-12-05 17:45:52 +0400

    remove unused companion objects for csv.package._

--- src/main/scala/csv/package.scala
+++ src/main/scala/csv/package.scala
@@ -15,10 +15,6 @@ package object csv {
     }
   }
 
-  object Row {
-    def empty: Row = new Row(Nil)
-  }
-
   final case class Rows(values: List[Row]) {
     override def toString: String = {
       val repr = values.map(_.toString).toString
@@ -31,16 +27,8 @@ package object csv {
       CSV.fromHeadersAndRows(headers, this)
   }
 
-  object Rows {
-    def empty: Rows = new Rows(Nil)
-  }
-
   final case class Header(value: String) {
     override def toString: String = value
-  }
-
-  object Header {
-    def empty: Header = new Header("")
   }
 
   final case class Headers(values: List[Header]) {
@@ -52,16 +40,8 @@ package object csv {
     }
   }
 
-  object Headers {
-    def empty: Headers = new Headers(Nil)
-  }
-
   final case class Content(data: String) {
     override def toString: String = data
-  }
-
-  object Content {
-    def empty: Content = new Content("")
   }
 
   final case class Column(header: Header, cells: List[Cell]) {
@@ -71,10 +51,6 @@ package object csv {
 
       s"$header: [$reprNormalized]"
     }
-  }
-
-  object Column {
-    def empty: Column = new Column(Header.empty, Nil)
   }
 
   final case class Columns(values: List[Column]) {
